899b5b0497a990b50c3af5d0460a57fd4c4ee10c
sinzek <chase.p.brock@gmail.com>
2026-03-27 23:27:24 -0500

    AssemblyInfo

--- QueueSmart.Api/obj/Debug/net10.0/QueueSmart.Api.AssemblyInfo.cs
+++ QueueSmart.Api/obj/Debug/net10.0/QueueSmart.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueSmart.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca30b9c7ccd2bff594fbde12ce4723485bb004f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb2e08b6f6df039a980b8375138688084f18c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueSmart.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueSmart.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
